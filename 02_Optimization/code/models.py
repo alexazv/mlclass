55ae7d99f7f15e622a14cf506076692ea9ec1d6b
Ubuntu 18 <alexandre.a512@gmail.com>
2019-06-12 16:11:13 -0700

    Fixed url, saving previous results

--- 02_Optimization/code/models.py
+++ 02_Optimization/code/models.py
@@ -3,7 +3,20 @@
 import random
 import requests
 import numpy as np
+import os
 
+results = {}
+
+
+def save_obj():
+    print("saving results")
+    np.savez_compressed('results.npz', results)
+
+
+def load_obj():
+    if os.path.isfile('./file.txt'):
+        np.load('results.npz', results)
+    print("Found {0} results".format(len(results)))
 
 class Antenna:
 
@@ -24,28 +37,28 @@ class Antenna:
 
     def function(self):
 
-        #print("phi: {0}".format(self.phi))
-        #print("theta: {0}".format(self.theta))
+        if (self.phi[0], self.phi[1], self.phi[2], self.theta[0], self.theta[1], self.theta[2]) in results:
+            print('found saved results')
+            return results[(self.phi[0], self.phi[1], self.phi[2], self.theta[0], self.theta[1], self.theta[2])]
 
-        url = "http://localhost:8080/antenna/simulate?phi1={0}&theta1={1}&phi2={2}&theta2={3}&phi3={4}&theta3={5}"\
-            .format(self.phi[0], self.theta[0], self.phi[1], self.theta[1], self.phi[2], self.theta[2])
+        url = "https://aydanomachado.com/mlclass/02_Optimization.php" \
+              "?dev_key=Alexandre%20Azevedo&phi1={0}& phi2={1}&phi3={2}&theta1={3}&theta2={4}&theta3={5}"\
+            .format(self.phi[0], self.phi[1], self.phi[2], self.theta[0], self.theta[1], self.theta[2])
 
         # Enviando requisição e salvando o objeto resposta
         r = requests.get(url=url)
         # Extraindo e imprimindo o texto da resposta
-        return float(r.text.split('\n', 1)[0])
+        data = float(r.json()['gain'])
+        results[(self.phi[0], self.phi[1], self.phi[2], self.theta[0], self.theta[1], self.theta[2])] = data
+        return data
 
     def neighbours(self, step):
         neighbours = []
 
-        for change in [[step, 0, 0], [0, step, 0], [0, 0, step]]:
+        for change in [[step, 0, 0], [0, step, 0], [0, 0, step],
+                       [step, step, 0], [step, 0, step], [0, step, step], [step, step, step]]:
             neighbours.append(Antenna(randomize=False, phi=np.add(self.phi, change), theta=self.theta))
             neighbours.append(Antenna(randomize=False, phi=np.add(self.phi, np.multiply(change, -1)), theta=self.theta))
             neighbours.append(Antenna(randomize=False, phi=self.phi, theta=np.add(self.theta, change)))
             neighbours.append(Antenna(randomize=False, phi=self.phi, theta=np.add(self.theta, np.multiply(change, -1))))
         return neighbours
-
-
-model = Antenna(randomize=True)
-response = model.function()
-print(response)
--- 02_Optimization/code/sim_ann.py
+++ 02_Optimization/code/sim_ann.py
@@ -1,4 +1,4 @@
-from models import Antenna
+from models import Antenna, load_obj, save_obj
 from copy import deepcopy
 import time
 import csv
@@ -7,6 +7,8 @@ import random
 
 
 def sim_ann(antenna0, stride, max_steps, max_unchanged):
+
+    load_obj()
 
     antenna = deepcopy(antenna0)
     old_function = antenna.function()
@@ -27,12 +29,17 @@ def sim_ann(antenna0, stride, max_steps, max_unchanged):
                 if old_function != function:
                     old_function = function
                     unchanged = 0
+
+        if step%5 == 0:
+            save_obj()
+
         results.append((antenna.phi, antenna.theta, old_function, chance))
         chance = chance * (0.95 ** ((step/max_steps)+1))
         unchanged += 1
         if unchanged >= max_unchanged:
+            save_obj()
             break
-
+    """
     with open("simm_ann_{0}.csv".format(time.time()), mode='w') as csv_file:
         fieldnames = ['phi1', 'phi2', 'phi3', 'theta1', 'theta2', 'theta3', 'result', 'temperature']
         writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
@@ -44,6 +51,9 @@ def sim_ann(antenna0, stride, max_steps, max_unchanged):
             writer.writerow({'phi1': result[0][0], 'phi2': result[0][1], 'phi3': result[0][2],
                              'theta1': result[1][0], 'theta2': result[1][1], 'theta3': result[1][2],
                              'result': result[2], 'temperature': results[3]})
+    """
+
+    save_obj()
 
 
-sim_ann(Antenna(randomize=True), stride=1, max_steps=1000, max_unchanged=10)
+sim_ann(Antenna(randomize=False, phi=[235, 0, 179], theta=[181, 187, 153]), stride=5, max_steps=1000, max_unchanged=5)
